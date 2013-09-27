4228e18c95237a54873f516c1f6a4e9ba64fefac
jeonghwan <ej88ej@gmail.com>
2013-09-27 11:47:16 +0900

    뷰포트 재설정
    뷰포트 재설정

--- facebook/redirect.php
+++ facebook/redirect.php
@@ -11,6 +11,7 @@ $loginUrl = $facebook->getLoginUrl(array(
     'scope'         => array('user_status', 'friends_photos'), // 퍼미션 조정 
     'redirect_uri'  => $redirect_url
 ));
+
 header('Location: '.$loginUrl);
 
 ?>
